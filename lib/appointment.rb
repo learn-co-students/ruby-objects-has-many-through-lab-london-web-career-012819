990d61a506aa64a2317104d30ae6cc95d8888598
Alexander Dubois <sander.dubois@outlook.com>
2019-02-01 14:00:11 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+
+class Appointment
+
+  attr_reader :date, :patient, :doctor
+
+  @@all = []
+
+  def initialize(patient, date, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
